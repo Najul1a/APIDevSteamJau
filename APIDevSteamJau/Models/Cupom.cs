e4b0583e0c1c8e98d268c17461d6fba9452fc330
Ana Geremias <anageremiias@gmail.com>
2025-04-29 14:29:35 -0300

    cupom add

---
+++ APIDevSteamJau/Models/Cupom.cs
@@ -0,0 +1,11 @@
+﻿public class Cupom
+{
+    public Guid CupomId { get; set; }
+    public string Nome { get; set; }
+    public decimal Desconto { get; set; }
+    public DateTime? DataValidade { get; set; }
+    public bool? Ativo { get; set; } = true;
+    public DateTime? DataCriacao { get; set; }
+    public int LimiteUso { get; set; }
+}
+
